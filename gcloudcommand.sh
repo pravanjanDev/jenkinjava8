f2a6c7d4c8faa6d299ec9f9b57d2c6100f0db533
pravanjaDev <pravanjan.niranjan@a-cti.com>
2018-01-18 23:33:20 +0530

    This is very good commit

---
+++ gcloudcommand.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+/Users/pravanjan/Documents/cloud/google-cloud-sdk/bin/gcloud auth activate-service-account jenkintojva8@jenkinjava8.iam.gserviceaccount.com --key-file Jenkinjava8-e2e9c92800ad.json
